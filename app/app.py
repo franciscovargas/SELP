97f45cf473fd0c07befdbf98f2c1bf9609d03724
francisco <f.vargas@icerobotics.com>
2014-11-26 15:06:33 +0000

    janitorial work change tabs to spaces to conform with pep8. Must add pep8 precommit hook

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-#imports
+# imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-	abort, render_template, flash
+    abort, render_template, flash
 from contextlib import closing
 
 # confifuration instructions
@@ -15,37 +15,40 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql',mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql',mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
 
 
 @app.before_request
 def before_request():
-	g.db = connect_db()
+    g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
-	db = getattr(g, 'db', None)
-	if db is not None:
-		db.close()
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def show_entries():
-	#cur = g.db.execute('select title, text from entries order by id desc')
-	#entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-	#return render_template('show_entries.html', entries=entries)
-	return render_template('constrainedmap.html')
+    # cur = g.db.execute('select title, text from entries order by id desc')
+    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    # return render_template('show_entries.html', entries=entries)
+    return render_template('constrainedmap.html')
 
 
-	
-	
+
+
 
 if __name__=='__main__':
 	app.run()
-
