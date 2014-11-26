53df6aecf22da01a21478f8ae516a482ff1df313
francisco <f.vargas@icerobotics.com>
2014-11-26 15:11:27 +0000

    janitorial work: spacing after commas and in between operators to conform with pep8 standards

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ def connect_db():
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql',mode='r') as f:
+        with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
             db.commit()
 
@@ -47,8 +47,5 @@ def show_entries():
     return render_template('constrainedmap.html')
 
 
-
-
-
-if __name__=='__main__':
-	app.run()
+if __name__ == '__main__':
+    app.run()
