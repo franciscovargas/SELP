95e2cb8419a51a2f512a45a6ad41393d8787f886
francisco <f.vargas@icerobotics.com>
2014-12-14 22:26:48 +0000

    implemented test which check that all edges in the database are less than 1km since this is what they actually are

--- test/test_distance.py
+++ test/test_distance.py
@@ -1,6 +1,7 @@
 from numpy import arange, pi
 import unittest
 from app.map_graph import distance
+import sqlite3
 
 class TestSequenceFunctions(unittest.TestCase):
 
@@ -9,6 +10,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.long1 = arange(0,2*pi,0.001*pi)*180/pi
         self.lat2 = arange(0,2*pi,0.001*pi)*180/pi
         self.long2 = arange(0,2*pi,0.001*pi)*180/pi
+        self.con = sqlite3.connect("app.db")
 
     def test_distance_does_not_raise_on_valid_input(self):
         raised = False
@@ -20,9 +22,20 @@ class TestSequenceFunctions(unittest.TestCase):
                                                  self.long2)]
         except:
             raised = True
-            raise
         self.assertFalse(raised, 'Exception raised')
 
+    def test_distance_value(self):
+        self.con.create_function("distance", 4, distance)
+        cur = self.con.cursor()
+        cur.execute("""SELECT distance(lat_start,
+                                       long_start,
+                                       lat_end,
+                                       long_end)
+                        FROM edges""")
+        results = cur.fetchall()
+        # print results
+        for dist in results:
+            assert abs(dist[0]) <= 1
 
 
 if __name__ == '__main__':
