3e28090e1864673480e095579da1fbd34df71aa7
francisco <f.vargas@icerobotics.com>
2014-12-15 01:36:16 +0000

    debugged visual test for the dice algorithm

--- test/visual_dice_test.py
+++ test/visual_dice_test.py
@@ -1,14 +1,25 @@
 from random import uniform
 import matplotlib.pyplot as plt
 from app.map_graph import decision_at_node_N
-r = uniform(0, 1.0)
-weights = (20, 30, 100, 100, 20, 30, 10)
+
+
+weights = (20, 30, 100, 100, 30, 20)
+weights2 = (20, 20, 20, 20, 20, 20)
 test = []
 test2 = []
-for j in range(100,10000,100):
+
+
+for j in range(100,10000,1000):
 	for i in range(j):
 	    test += [decision_at_node_N(weights)]
-	    # test2 += [decision_at_node(probabilities)]
 	plt.figure()
 	n, bins, patches = plt.hist(test, normed=True)
-	plt.savefig('test%s.png'%j)
+	plt.savefig('test_normal%s.png'%j)
+
+
+for j in range(100,10000,1000):
+	for i in range(j):
+	    test2 += [decision_at_node_N(weights2)]
+	plt.figure()
+	n, bins, patches = plt.hist(test2, normed=True)
+	plt.savefig('test_uniform%s.png'%j)
